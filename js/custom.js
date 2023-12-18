5283b84bbc92d04df7cdade4a81893b9d3a387af
Sohail <141889848+razadevloper@users.noreply.github.com>
2023-12-18 16:56:43 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -27,3 +27,16 @@ window.onscroll = function() {
       navbar.style.backgroundColor = "";
     }
   };
+
+
+
+  window.addEventListener("load", () => {
+    const bar = document.querySelectorAll(".bar");
+    for (let i = 0; i < bar.length; i++) {
+      bar.forEach((item, j) => {
+        // Random move
+        item.style.animationDuration = `${Math.random() * (0.7 - 0.2) + 0.2}s`; // Change the numbers for speed / ( max - min ) + min / ex. ( 0.5 - 0.1 ) + 0.1
+      });
+    }
+  });
+  
